b987fcf60decd621354f723f2c7d2a0b1e9812f3
Khawlah Khalid <khawlah1920@hotmail.com>
2023-11-23 12:43:16 +0300

    Look Around Scene

--- iOS17Map/ContentView.swift
+++ iOS17Map/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import MapKit
+
+let coordinates = CLLocationCoordinate2D(latitude: 37.334967, longitude:  -121.892566)
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var cameraPosition: MapCameraPosition = .region(.init(center: coordinates, latitudinalMeters: 1200, longitudinalMeters: 1200))
+    
+    @State var lookAroundScene: MKLookAroundScene?{
+        didSet{
+            if let _ = lookAroundScene{
+                isShowingLookAroundScene = true
+            }
         }
-        .padding()
+    }
+    
+    @State var isShowingLookAroundScene: Bool = false
+    
+    var body: some View {
+        Map(initialPosition: cameraPosition){
+            Annotation("Some location", coordinate: coordinates) {
+                Image(systemName: "mappin.circle.fill")
+                    .font(.title)
+                    .foregroundStyle(.pink.gradient)
+                    .onTapGesture {
+                        getLookAroundScene(for: coordinates)
+                    }
+            }
+        }
+        .lookAroundViewer(isPresented: $isShowingLookAroundScene,
+                          scene: $lookAroundScene)
+        
+        
+    }
+    
+    func getLookAroundScene(for location: CLLocationCoordinate2D){
+        Task{
+            let request = MKLookAroundSceneRequest(coordinate: location)
+            lookAroundScene = try? await request.scene
+        }
     }
 }
 
