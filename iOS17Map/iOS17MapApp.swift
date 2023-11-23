bbb25177d21fc61aa4957defca044aae9667d4f4
Khawlah Khalid <khawlah1920@hotmail.com>
2023-11-23 11:47:40 +0300

    Initial Commit

---
+++ iOS17Map/iOS17MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS17MapApp.swift
+//  iOS17Map
+//
+//  Created by Khawlah Khalid on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS17MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
